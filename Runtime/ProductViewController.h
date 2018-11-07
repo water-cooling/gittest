2f7541e3c85bd29394dd3b9cc920f5280ac9aca4
humengfan <1025590386@qq.com>
2018-11-07 18:10:14 +0800

    支线变更

---
+++ Runtime/ProductViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProductViewController.h
+//  Runtime
+//
+//  Created by humengfan on 2018/11/7.
+//  Copyright © 2018年 王师傅 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProductViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Runtime/ProductViewController.m
@@ -0,0 +1,33 @@
+//
+//  ProductViewController.m
+//  Runtime
+//
+//  Created by humengfan on 2018/11/7.
+//  Copyright © 2018年 王师傅 Mac. All rights reserved.
+//
+
+#import "ProductViewController.h"
+
+@interface ProductViewController ()
+
+@end
+
+@implementation ProductViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"test";
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
